c37359d5db67f203be3265fb94f07643c5792eda
stellalagat <stellalagat058@gmail.com>
2025-10-01 09:56:27 +0300

    h

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const cors = require('cors');
 const { body, validationResult } = require('express-validator');
 require('dotenv').config();
 
-const express = require("express");
-const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
